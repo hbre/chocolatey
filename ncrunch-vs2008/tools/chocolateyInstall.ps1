c65680776437cbd608c0df3d13fd9ff2ca0d06dc
Henning Bredenkötter <hbre@users.noreply.github.com>
2019-04-02 07:44:50 +0000

    Added docsUrl and bugTrackerUrl

--- ncrunch-vs2008/tools/chocolateyInstall.ps1
+++ ncrunch-vs2008/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName='ncrunch-vs2008'
-$url32='https://s3.amazonaws.com/downloads.ncrunch.net/NCrunch_VS2008_3.24.0.2.msi'
-$checksum32='ABC'
+$url32='https://s3.amazonaws.com/downloads.ncrunch.net/NCrunch_VS2008_3.25.0.3.msi'
+$checksum32='BDEEED8D267EF95487C5D50242260E09E100DAC216B8C6EBE6B08BDFB9E1D1DF'
  
 $ErrorActionPreference = 'Stop'
  
