1e2e56fd78b78284927885f254dbcd7f72a2699a
Henning Bredenkötter <hbre@users.noreply.github.com>
2019-04-02 07:53:04 +0000

    Updated Packages to Install without S3

--- ncrunch-vs2008/tools/chocolateyInstall.ps1
+++ ncrunch-vs2008/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName='ncrunch-vs2008'
-$url32='https://s3.amazonaws.com/downloads.ncrunch.net/NCrunch_VS2008_3.25.0.3.msi'
+$url32='http://downloads.ncrunch.net/NCrunch_VS2008_3.25.0.3.msi'
 $checksum32='BDEEED8D267EF95487C5D50242260E09E100DAC216B8C6EBE6B08BDFB9E1D1DF'
  
 $ErrorActionPreference = 'Stop'
--- ncrunch-vs2019/tools/chocolateyInstall.ps1
+++ ncrunch-vs2019/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName='ncrunch-vs2019'
-$url32='https://s3.amazonaws.com/downloads.ncrunch.net/NCrunch_VS2019_3.25.0.3.msi'
+$url32='http://downloads.ncrunch.net/NCrunch_VS2019_3.25.0.3.msi'
 $checksum32='A3C0FC98FC019B2862B4E2785A66ABB0094A51BB53CF2C6B425C107F5F3234EC'
  
 $ErrorActionPreference = 'Stop'
