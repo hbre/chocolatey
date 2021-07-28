9048c2e453517b2c39ad7f3005ce043270a480d9
Henning Bredenkötter <hbre@users.noreply.github.com>
2021-07-28 08:42:44 +0200

    NCrunch vs2022

---
+++ ncrunch-vs2022/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName='ncrunch-vs2022'
+$url32='http://downloads.ncrunch.net/NCrunch_VS2022_4.9.0.8.msi'
+$checksum32='1E4FF9E20809D454E41BDDAADFAB83873671E51C8F93E468092B138EC4BBCAE4'
+ 
+$ErrorActionPreference = 'Stop'
+ 
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'MSI'
+  url                    = $url32
+  checksum               = $checksum32
+  checksumType           = 'sha256'
+  validExitCodes         = @(0)  
+  silentArgs             = '/quiet'
+}
+
+Install-ChocolateyPackage @packageArgs
