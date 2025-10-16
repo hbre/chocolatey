775796594d913935dd60579b7825323dd2abae6d
Sean Killeen <SeanKilleen@gmail.com>
2025-10-16 16:15:19 -0400

    Update SHA256 file hash

--- ncrunch-vs2026/tools/chocolateyInstall.ps1
+++ ncrunch-vs2026/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'ncrunch-vs2026'
 $url32 = 'https://s3.amazonaws.com/downloads.ncrunch.net/NCrunch_VS2026_5.18.0.2.msi'
-$checksum32 = 'E6C48EC9F67D944503D795D592C17EEDCB73E31ECDDFCFA4C8A919E3DBED6E5A'
+$checksum32 = 'F1133BE2D64FFD68E988BC3FD9F4A4AF4AFB280704BD961A83E8B5E77C7BDF1F'
  
 $ErrorActionPreference = 'Stop'
  
