b2cfadba3483bc947c9a012de5808737cf5eec53
Sean Killeen <SeanKilleen@gmail.com>
2025-10-16 16:14:27 -0400

    update package name and download link
    verified the link first

--- ncrunch-vs2026/tools/chocolateyInstall.ps1
+++ ncrunch-vs2026/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'ncrunch-vs2022'
-$url32 = 'https://s3.amazonaws.com/downloads.ncrunch.net/NCrunch_VS2022_5.16.0.5.msi'
+$packageName = 'ncrunch-vs2026'
+$url32 = 'https://s3.amazonaws.com/downloads.ncrunch.net/NCrunch_VS2026_5.18.0.2.msi'
 $checksum32 = 'E6C48EC9F67D944503D795D592C17EEDCB73E31ECDDFCFA4C8A919E3DBED6E5A'
  
 $ErrorActionPreference = 'Stop'
